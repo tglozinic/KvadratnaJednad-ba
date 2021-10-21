cd7b0c1ee3ca41b5e99645216fa50bdea0f2fbb7
tglozinic <tglozinic@dspace.hr>
2021-10-21 18:54:14 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KvadratnaJednadžba
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            QuadraticEquation qe = new QuadraticEquation(1, 2, -3);
+            var roots = qe.Roots;
+            Console.WriteLine(roots[0]);
+            Console.WriteLine(roots[1]);
+
+             qe = new QuadraticEquation(-1, 2, -1);
+             roots = qe.Roots;
+            Console.WriteLine(roots[0]);
+            Console.WriteLine(roots[1]);
+
+            Console.ReadKey();
+
+        }
+    }
+}
